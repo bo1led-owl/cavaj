d3cf5aad957c7d4f327219e5c94c0605b88e93c8
Ilya Sovtsov <ilyasovty@gmail.com>
2025-11-07 19:11:51 +0700

    add destination to instructions

--- src/main/scala/ir/Value.scala
+++ src/main/scala/ir/Value.scala
@@ -3,15 +3,17 @@ package ir
 
 trait Value
 
-case object NullLit                   extends Value
-case class ByteLit(value: Byte)       extends Value
-case class ShortLit(value: Short)     extends Value
-case class IntLit(value: Int)         extends Value
-case class LongLit(value: Long)       extends Value
-case class CharLit(value: Char)       extends Value
-case class BooleanLit(value: Boolean) extends Value
-case class FloatLit(value: Float)     extends Value
-case class DoubleLit(value: Double)   extends Value
+sealed trait Literal extends Value
 
-case object ThisRef             extends Value
+case object NullLit                   extends Literal
+case class ByteLit(value: Byte)       extends Literal
+case class ShortLit(value: Short)     extends Literal
+case class IntLit(value: Int)         extends Literal
+case class LongLit(value: Long)       extends Literal
+case class CharLit(value: Char)       extends Literal
+case class BooleanLit(value: Boolean) extends Literal
+case class FloatLit(value: Float)     extends Literal
+case class DoubleLit(value: Double)   extends Literal
+case object ThisRef                   extends Literal
+
 case class Variable(index: Int) extends Value
