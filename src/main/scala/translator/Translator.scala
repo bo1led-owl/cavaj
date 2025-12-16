32d88532a5dc5bf08d7caf0f0626cb061fa593f5
EugeneKornev <yevgene.kornev@gmail.com>
2025-12-16 07:53:08 +0300

    Trans start

--- src/main/scala/translator/Translator.scala
+++ src/main/scala/translator/Translator.scala
@@ -6,24 +6,18 @@ import ir.*
 
 import ast.*
 
-//def translate(stmt: IfStmt): String =
-//  s"if (${stmt.cond}) {\n\t${stmt.onTrue}} ${if stmt.onFalse.isDefined then s"""else ${stmt.onFalse}\n}""" else """"""}"
-//
-//
-//def translate(stmt: WhileStmt): String =
-//  s"while (${stmt.cond}) {\n\t${stmt.body}}"
 
-def translate(stmt: Stmt): String =
+def translateStmt(stmt: Stmt): String =
   stmt match
     case VarDeclStmt(ty, name, value) => s"$ty $name = $value"
     case ExprStmt(value) => ""
-    case BlockStmt(body) => s"{${body.map(translate).mkString(";\n")}}"
+    case BlockStmt(body) => s"{${body.map(translateStmt).mkString(";\n")}}"
     case IfStmt(cond, onTrue, onFalse) => s"if ($cond) \n\t$onTrue ${onFalse.map("else " + _).getOrElse("")}"
     case WhileStmt(cond, body) => s"while ($cond) $body"
     case DoWhileStmt(cond, body) => s"do $body while ($cond)"
     case ForStmt(init, cond, step, body) => {
       val initStr = init match
-        case v: VarDeclStmt => translate(v)
+        case v: VarDeclStmt => translateStmt(v)
         case s: Seq[?] => s.mkString(", ")
 
       ???
@@ -35,16 +29,16 @@ def translate(stmt: Stmt): String =
 
 
 
-def translate(method: AstMethod): String = {
+def translateMethod(method: AstMethod): String = {
   val quals = method.qualifiers.filter(_ != Qualifier.Default).mkString("  ", " ", " ")
   val params = method.parameters.map { case (name, ty) => s"$ty $name" }.mkString(", ")
-  val bodys = method.body.map(s => s"{\n${translate(s)}\n}").getOrElse("")
+  val bodys = method.body.map(s => s"{\n${translateStmt(s)}\n}").getOrElse("")
   s"$quals${method.rettype}${method.name}($params)$bodys"
 }
 
 
 
-def translate(Class: AstClass): String = {
+def translateClass(Class: AstClass): String = {
   val quals = Class.qualifiers.filter(_ != Qualifier.Default).mkString("", " ", " ")
   val extendss = if Class.extendsClass.isDefined then s" extends ${Class.extendsClass}" else ""
   val implementss = if Class.implements.nonEmpty then Class.implements.mkString(", ") else ""
@@ -54,7 +48,7 @@ def translate(Class: AstClass): String = {
 }
 
 
-def translate(interface: AstInterface): String = {
+def translateInterface(interface: AstInterface): String = {
   val quals = interface.qualifiers.filter(_ != Qualifier.Default).mkString("", " ", " ")
   val fieldss = interface.fields.values.map(" " + _.toString + ";").mkString("\n")
   val methodss = interface.methods.values.flatten.map(_.toString).mkString("\n\n")
@@ -62,8 +56,8 @@ def translate(interface: AstInterface): String = {
   s"${quals}interface ${interface.name}$extendss {\n$fieldss\n\n$methodss\n}"
 }
 
-def translate(packagee: AstPackage): String = {
-  val interfaces = packagee.interfaces.values.map(i => translate(i: AstInterface)).mkString("\n\n")
-  val classes = packagee.classes.values.map(i => translate(i: AstClass)).mkString("\n\n")
+def translatePackage(packagee: AstPackage): String = {
+  val interfaces = packagee.interfaces.values.map(i => translateInterface(i)).mkString("\n\n")
+  val classes = packagee.classes.values.map(i => translateClass(i)).mkString("\n\n")
   s"$interfaces\n\n$classes"
 }
