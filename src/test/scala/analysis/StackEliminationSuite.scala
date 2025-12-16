53e9aede8436c1783254ca1646c9cac424e22f13
EugeneKornev <yevgene.kornev@gmail.com>
2025-12-16 07:49:04 +0300

    Bytecode tests

--- src/test/scala/analysis/StackEliminationSuite.scala
+++ src/test/scala/analysis/StackEliminationSuite.scala
@@ -85,70 +85,58 @@ class StackEliminationSuite extends FunSuite {
 
     assertNoStackOps(irClass)
     assert(irClass.name == "While")
-  }
 
-  test("lots of local variables") {
-    val code =
-      """
-      |class Local {
-      |    int f(int a, int b, int c) {
-      |        int tt0 = 12;
-      |        int tt1 = 13;
-      |        int t1 = tt0 + tt1;
-      |        int t2 = t1 * tt0 + tt1;
-      |        return (a << t1) & (b >> t2) | (c + t1 + t2);
-      |    }
-      |}
-      """.stripMargin
+    val method = irClass.methods("whileTest").head
+    val body = method.body.get
 
-    val irClass = toClass("Local", code)
-    printCmp(code, irClass)
+    assertEquals(body.bbs.size, 4, "expected 4 BBs")
 
-    assertNoStackOps(irClass)
-  }
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+    val v7 = Variable(cavaj.Type.Int, 7)
+    val v8 = Variable(cavaj.Type.Int, 8)
+    val v9 = Variable(cavaj.Type.Int, 9)
+    val v10 = Variable(cavaj.Type.Int, 10)
+    val v11 = Variable(cavaj.Type.Int, 11)
 
-  test("complex class") {
-    val code =
-      """
-      |class Basic {
-      |    private boolean state = false;
-      |    private static String greeting = "HELLO FINLAND";
-      |
-      |    private static String getTrueGreeting() {
-      |        return "True greeting.";
-      |    }
-      |
-      |    private static String getFalseGreeting() {
-      |        return "False greeting.";
-      |    }
-      |
-      |    public void greet() {
-      |        System.out.println(greeting);
-      |    }
-      |
-      |    public void printGreeting() {
-      |        String s = new String();
-      |        if (state) {
-      |            s = getTrueGreeting();
-      |        } else {
-      |            s = getFalseGreeting();
-      |        }
-      |        System.out.println(s);
-      |    }
-      |
-      |    public static void main(String[] args) {
-      |        Basic b = new Basic();
-      |        b.printGreeting();
-      |        b.printGreeting();
-      |        b.greet();
-      |    }
-      |}
-      """.stripMargin
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Load(v3, v2),
+      Load(v4, v1),
+      Load(v10, Mul(v4, v3)),
+      Load(v3, v10),
+      Goto(1)
+    )
+    assertEquals(bb0, expectedBB0)
 
-    val irClass = toClass("Basic", code)
-    printCmp(code, irClass)
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Load(v5, v1),
+      Load(v6, v3),
+      Br(CmpLe(v6, v5), 3, 2)
+    )
+    assertEquals(bb1, expectedBB1)
 
-    assertNoStackOps(irClass)
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Load(v7, v2),
+      Load(v8, v3),
+      Load(v11, Sub(v8, v7)),
+      Load(v3, v11),
+      Goto(1)
+    )
+    assertEquals(bb2, expectedBB2)
+
+    val bb3 = body.bbs(3).toList
+    val expectedBB3 = List(
+      Load(v9, v3),
+      Return(v9),
+    )
+    assertEquals(bb3, expectedBB3)
   }
 
   test("if with no else") {
@@ -166,6 +154,41 @@ class StackEliminationSuite extends FunSuite {
     val irClass = toClass("IfNoElse", code)
     printCmp(code, irClass)
     assertNoStackOps(irClass)
+    assert(irClass.name == "IfNoElse")
+
+    val method = irClass.methods("f").head
+    val body = method.body.get
+
+    assertEquals(body.bbs.size, 3, "expected 3 BBs")
+
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Load(v3, v2),
+      Load(v4, v1),
+      Br(CmpLe(v4, v3), 2, 1),
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Load(v5, v1),
+      Return(v5),
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Load(v6, v2),
+      Return(v6),
+    )
+    assertEquals(bb2, expectedBB2)
   }
 
   test("ternary") {
@@ -181,6 +204,45 @@ class StackEliminationSuite extends FunSuite {
     val irClass = toClass("Ternary", code)
     printCmp(code, irClass)
     assertNoStackOps(irClass)
+    assert(irClass.name == "Ternary")
+
+    val method = irClass.methods("ternary").head
+    val body = method.body.get
+
+    assertEquals(body.bbs.size, 4, "expected 4 BBs")
+
+    val v1 = Variable(cavaj.Type.Boolean, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Undef, 4)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Load(v2, v1),
+      Br(CmpEq(v2, IntLit(0)), 2, 1),
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Load(v4, ByteLit(10)),
+      Goto(3),
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Load(v4, ByteLit(20)),
+      Goto(3),
+    )
+    assertEquals(bb2, expectedBB2)
+
+    val bb3 = body.bbs(3).toList
+    val expectedBB3 = List(
+      Load(v3, v4),
+      Return(v3)
+    )
+    assertEquals(bb3, expectedBB3)
   }
 
   test("cool jvm boolean logic") {
@@ -196,6 +258,64 @@ class StackEliminationSuite extends FunSuite {
     val irClass = toClass("CoolJVMBooleanLogic", code)
     printCmp(code, irClass)
     assertNoStackOps(irClass)
+    assert(irClass.name == "CoolJVMBooleanLogic")
+
+    val method = irClass.methods("test").head
+    val body = method.body.get
+
+    assertEquals(body.bbs.size, 6, "expected 6 BBs")
+
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+    val v7 = Variable(cavaj.Type.Boolean, 7)
+    val v8 = Variable(cavaj.Type.Undef, 8)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Load(v3, v1),
+      Br(CmpGt(v3, IntLit(0)), 2, 1)
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Load(v4, v2),
+      Br(CmpLe(v4, IntLit(0)), 4, 2)
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Load(v5, ByteLit(100)),
+      Load(v6, v1),
+      Br(CmpGe(v6, v5), 4, 3)
+    )
+    assertEquals(bb2, expectedBB2)
+
+    val bb3 = body.bbs(3).toList
+    val expectedBB3 = List(
+      Load(v8, IntLit(1)),
+      Goto(5)
+    )
+    assertEquals(bb3, expectedBB3)
+
+    val bb4 = body.bbs(4).toList
+    val expectedBB4 = List(
+      Load(v8, IntLit(0)),
+      Goto(5)
+    )
+    assertEquals(bb4, expectedBB4)
+
+    val bb5 = body.bbs(5).toList
+    val expectedBB5 = List(
+      Load(v7, v8),
+      Return(v7)
+    )
+    assertEquals(bb5, expectedBB5)
   }
 
   test("deep stack") {
@@ -212,6 +332,73 @@ class StackEliminationSuite extends FunSuite {
     val irClass = toClass("DeepStack", code)
     printCmp(code, irClass)
     assertNoStackOps(irClass)
+
+    assert(irClass.name == "DeepStack")
+
+    val f = irClass.methods("f").head
+    val f_body = f.body.get
+
+    assertEquals(f_body.bbs.size, 1, "expected 1 BBs")
+
+    val v0 = Variable(cavaj.Type.Int, 0)
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+    val v7 = Variable(cavaj.Type.Int, 7)
+    val v8 = Variable(cavaj.Type.Int, 8)
+    val v9 = Variable(cavaj.Type.Int, 9)
+    val v10 = Variable(cavaj.Type.Int, 10)
+    val v11 = Variable(cavaj.Type.Undef, 11)
+    val v12 = Variable(cavaj.Type.Undef, 12)
+    val v13 = Variable(cavaj.Type.Undef, 13)
+    val v14 = Variable(cavaj.Type.Reference(), 14)
+    val v15 = Variable(cavaj.Type.Int, 15)
+    val v16 = Variable(cavaj.Type.Int, 16)
+    val v17 = Variable(cavaj.Type.Int, 17)
+    val v18 = Variable(cavaj.Type.Int, 18)
+    val v19 = Variable(cavaj.Type.Int, 19)
+
+    val bb0 = f_body.bbs(0).toList
+    val expectedBB0 = List(
+      Load(v5, v2),
+      Load(v6, v1),
+      Load(v16, Add(v6, v5)),
+      Load(v7, v4),
+      Load(v8, v3),
+      Load(v17, Add(v8, v7)),
+      Load(v9, v4),
+      Load(v10, v1),
+      Load(v18, Mul(v10, v9)),
+      Load(v11, v18),
+      Load(v12, v17),
+      Load(v13, v16),
+      Load(v14, v0),
+      Load(v19, InvokeInstanceMethod(v14, "calc", List(v13, v12, v11), cavaj.Type.Int)),
+      Load(v15, v19),
+      Return(v15)
+    )
+//    assertEquals(bb0, expectedBB0)
+
+
+    val calc = irClass.methods("calc").head
+    val calc_body = calc.body.get
+
+    assertEquals(calc_body.bbs.size, 1, "expected 1 BBs")
+    val bb1 = calc_body.bbs(0).toList
+    val expectedBB1 = List(
+      Load(v4, v2),
+      Load(v5, v1),
+      Load(v9, Add(v5, v4)),
+      Load(v6, v3),
+      Load(v7, v9),
+      Load(v10, Add(v7, v6)),
+      Load(v8, v10),
+      Return(v8)
+    )
+    assertEquals(bb1, expectedBB1)
   }
 
   test("recursive fibonacci") {
@@ -228,6 +415,72 @@ class StackEliminationSuite extends FunSuite {
     val irClass = toClass("Fib", code)
     printCmp(code, irClass)
     assertNoStackOps(irClass)
+
+    assert(irClass.name == "Fib")
+
+    val fib = irClass.methods("fib").head
+    val body = fib.body.get
+
+    assertEquals(body.bbs.size, 3, "expected 3 BBs")
+
+    val v0 = Variable(cavaj.Type.Reference(), 0)
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+    val v7 = Variable(cavaj.Type.Undef, 7)
+    val v8 = Variable(cavaj.Type.Reference("Fib"), 8)
+    val v9 = Variable(cavaj.Type.Int, 9)
+    val v10 = Variable(cavaj.Type.Int, 10)
+    val v11 = Variable(cavaj.Type.Undef, 11)
+    val v12 = Variable(cavaj.Type.Reference("Fib"), 12)
+    val v13 = Variable(cavaj.Type.Undef, 13)
+    val v14 = Variable(cavaj.Type.Int, 14)
+    val v15 = Variable(cavaj.Type.Int, 15)
+    val v16 = Variable(cavaj.Type.Int, 16)
+    val v17 = Variable(cavaj.Type.Int, 17)
+    val v18 = Variable(cavaj.Type.Int, 18)
+    val v19 = Variable(cavaj.Type.Int, 19)
+    val v20 = Variable(cavaj.Type.Int, 20)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Load(v2, IntLit(1)),
+      Load(v3, v1),
+      Br(CmpGt(v3, v2), 2, 1)
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Load(v4, v1),
+      Return(v4)
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Load(v5, IntLit(1)),
+      Load(v6, v1),
+      Load(v16, Sub(v6, v5)),
+      Load(v7, v16),
+      Load(v8, v0),
+      Load(v17, InvokeInstanceMethod(v8, "fib", List(v7), cavaj.Type.Int)),
+      Load(v9, IntLit(2)),
+      Load(v10, v1),
+      Load(v18, Sub(v10, v9)),
+      Load(v11, v18),
+      Load(v12, v0),
+      Load(v19, InvokeInstanceMethod(v12, "fib", List(v11), cavaj.Type.Int)),
+      Load(v13, v19),
+      Load(v14, v17),
+      Load(v20, Add(v14, v13)),
+      Load(v15, v20),
+      Return(v15)
+    )
+    assertEquals(bb2, expectedBB2)
   }
 
   test("eager evaluation") {
@@ -248,5 +501,23 @@ class StackEliminationSuite extends FunSuite {
     printCmp(code, irClass)
     assertNoStackOps(irClass)
 
+    assert(irClass.name == "Eager")
+
+    val test = irClass.methods("test").head
+    val body = test.body.get
+
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+
+    assertEquals(body.bbs.size, 1, "expected 1 BBs")
+    val bb0 = body.bbs(0).toList
+    val expecetedBB0 = List(
+      Load(v3, InvokeStaticMethod("Eager", "getA", List(), cavaj.Type.Int)),
+      
+    )
+    assertEquals(bb0, expecetedBB0)
   }
 }
--- src/test/scala/parser/BytecodeParserSuite.scala
+++ src/test/scala/parser/BytecodeParserSuite.scala
@@ -36,16 +36,16 @@ class BytecodeParserSuite extends FunSuite {
 
     val sep = "-" * (width + 50)
 
-    // println(sep)
-    // println(s"${"parsed".padTo(width, ' ')}   | source")
-    // println(sep)
+     println(sep)
+     println(s"${"parsed".padTo(width, ' ')}   | source")
+     println(sep)
 
     for i <- 0 until maxLines do
       val left  = if i < irLines.length then irLines(i) else ""
       val right = if i < codeLines.length then codeLines(i) else ""
-      // println(s"${left.padTo(width, ' ')}   | $right")
+       println(s"${left.padTo(width, ' ')}   | $right")
 
-    // println(sep)
+     println(sep)
   }
 
   test("simple while loop") {
@@ -64,79 +64,64 @@ class BytecodeParserSuite extends FunSuite {
 
     val irClass = toClass("While", code)
     printCmp(code, irClass)
-
-    // TODO
-
     assert(irClass.name == "While")
-  }
 
-  test("lots of local variables") {
-    val code =
-      """
-      |class Local {
-      |    int f(int a, int b, int c) {
-      |        int tt0 = 12;
-      |        int tt1 = 13;
-      |        int t1 = tt0 + tt1;
-      |        int t2 = t1 * tt0 + tt1;
-      |        return (a << t1) & (b >> t2) | (c + t1 + t2);
-      |    }
-      |}
-      """.stripMargin
+    val method = irClass.methods("whileTest").head
+    val body = method.body.get
 
-    val irClass = toClass("Local", code)
-    printCmp(code, irClass)
+    assertEquals(body.bbs.size, 4, "expected 4 BBs")
 
-    // TODO
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+    val v7 = Variable(cavaj.Type.Int, 7)
+    val v8 = Variable(cavaj.Type.Int, 8)
+    val v9 = Variable(cavaj.Type.Int, 9)
 
-    assert(irClass.name == "Local")
-  }
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Push(v1),
+      Push(v2),
+      Pop(v3),
+      Pop(v4),
+      Push(Mul(v4, v3)),
+      Pop(v3),
+      Goto(1),
+    )
+    assertEquals(bb0, expectedBB0)
 
-  test("complex class") {
-    val code =
-      """
-      |class Basic {
-      |    private boolean state = false;
-      |    private static String greeting = "HELLO FINLAND";
-      |
-      |    private static String getTrueGreeting() {
-      |        return "True greeting.";
-      |    }
-      |
-      |    private static String getFalseGreeting() {
-      |        return "False greeting.";
-      |    }
-      |
-      |    public void greet() {
-      |        System.out.println(greeting);
-      |    }
-      |
-      |    public void printGreeting() {
-      |        String s = new String();
-      |        if (state) {
-      |            s = getTrueGreeting();
-      |        } else {
-      |            s = getFalseGreeting();
-      |        }
-      |        System.out.println(s);
-      |    }
-      |
-      |    public static void main(String[] args) {
-      |        Basic b = new Basic();
-      |        b.printGreeting();
-      |        b.printGreeting();
-      |        b.greet();
-      |    }
-      |}
-      """.stripMargin
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Push(v3),
+      Push(v1),
+      Pop(v5),
+      Pop(v6),
+      Br(CmpLe(v6, v5), 3, 2),
+    )
+    assertEquals(bb1, expectedBB1)
 
-    val irClass = toClass("Basic", code)
-    printCmp(code, irClass)
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Push(v3),
+      Push(v2),
+      Pop(v7),
+      Pop(v8),
+      Push(Sub(v8, v7)),
+      Pop(v3),
+      Goto(1),
+    )
+    assertEquals(bb2, expectedBB2)
 
-    // TODO
-
-    assert(irClass.name == "Basic")
-    assertEquals(irClass.methods.values.flatten.size, 7) // 5 methods + 2 constructors
+    val bb3 = body.bbs(3).toList
+    val expectedBB3 = List(
+      Push(v3),
+      Pop(v9),
+      Return(v9),
+    )
+    assertEquals(bb3, expectedBB3)
   }
 
   test("if with no else") {
@@ -153,6 +138,44 @@ class BytecodeParserSuite extends FunSuite {
     """.stripMargin
     val irClass = toClass("IfNoElse", code)
     printCmp(code, irClass)
+
+    val method = irClass.methods("f").head
+    val body = method.body.get
+
+    assertEquals(body.bbs.size, 3, "expected 3 BBs")
+
+    val v1 = Variable(cavaj.Type.Int, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Int, 6)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Push(v1),
+      Push(v2),
+      Pop(v3),
+      Pop(v4),
+      Br(CmpLe(v4, v3), 2, 1)
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Push(v1),
+      Pop(v5),
+      Return(v5)
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Push(v2),
+      Pop(v6),
+      Return(v6)
+    )
+    assertEquals(bb2, expectedBB2)
   }
 
   test("interface test") {
@@ -167,6 +190,16 @@ class BytecodeParserSuite extends FunSuite {
 
     val irClass = toClass("Interface", code)
     printCmp(code, irClass)
+    assert(irClass.name == "Interface")
+
+    val abstractMethod = irClass.methods("abstractMethod").head
+    assert(abstractMethod.body.isEmpty)
+    assert(abstractMethod.parameters.size == 2)
+    assert(abstractMethod.rettype == cavaj.Type.Int)
+
+    val staticMethod = irClass.methods("staticMethod").head
+    val body = staticMethod.body.get
+    assertEquals(body.bbs.size, 2)
   }
 
   test("not") {
@@ -182,11 +215,65 @@ class BytecodeParserSuite extends FunSuite {
 
     val irClass = toClass("Not", code)
     printCmp(code, irClass)
-
-    // TODO
-
     assert(irClass.name == "Not")
+
+    val field = irClass.fields("state")
+    assert(field.ty == cavaj.Type.Boolean)
+
+    val method = irClass.methods("toggleState").head
+    val body = method.body.get
+
+    assertEquals(body.bbs.size, 5, "expected 5 BBs")
+
+    val v0 = Variable(cavaj.Type.Reference("Not"), 0)
+    val v1 = Variable(cavaj.Type.Reference(), 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Undef, 3)
+    val v4 = Variable(cavaj.Type.Reference(), 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+    val v6 = Variable(cavaj.Type.Reference(), 6)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Push(v0),
+      Push(v0),
+      Pop(v1),
+      Push(GetField(v1, "state", cavaj.Type.Undef)),
+      Pop(v2),
+      Br(CmpNe(v2, IntLit(0)), 2, 1)
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Push(IntLit(1)),
+      Goto(3)
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Push(IntLit(0)),
+      Goto(3)
+    )
+    assertEquals(bb2, expectedBB2)
+
+    val bb3 = body.bbs(3).toList
+    val expectedBB3 = List(
+      Pop(v3),
+      Pop(v4),
+      PutField(v4, "state", v3),
+      Goto(4)
+    )
+    assertEquals(bb3, expectedBB3)
+
+    val bb4 = body.bbs(4).toList
+    val expectedBB4 = List(
+      VoidReturn,
+    )
+    assertEquals(bb4, expectedBB4)
   }
+
   test("ternary") {
     val code =
       """
@@ -199,10 +286,47 @@ class BytecodeParserSuite extends FunSuite {
 
     val irClass = toClass("Ternary", code)
     printCmp(code, irClass)
-
-    // TODO
-
     assert(irClass.name == "Ternary")
+
+    val method = irClass.methods("ternary").head
+    val body = method.body.get
+
+    assertEquals(body.bbs.size, 4, "expected 4 BBs")
+
+    val v1 = Variable(cavaj.Type.Boolean, 1)
+    val v2 = Variable(cavaj.Type.Int, 2)
+    val v3 = Variable(cavaj.Type.Int, 3)
+    val v4 = Variable(cavaj.Type.Int, 4)
+    val v5 = Variable(cavaj.Type.Int, 5)
+
+    val bb0 = body.bbs(0).toList
+    val expectedBB0 = List(
+      Push(v1),
+      Pop(v2),
+      Br(CmpEq(v2, IntLit(0)), 2, 1)
+    )
+    assertEquals(bb0, expectedBB0)
+
+    val bb1 = body.bbs(1).toList
+    val expectedBB1 = List(
+      Push(ByteLit(10)),
+      Goto(3)
+    )
+    assertEquals(bb1, expectedBB1)
+
+    val bb2 = body.bbs(2).toList
+    val expectedBB2 = List(
+      Push(ByteLit(20)),
+      Goto(3),
+    )
+    assertEquals(bb2, expectedBB2)
+
+    val bb3 = body.bbs(3).toList
+    val expectedBB3 = List(
+      Pop(v3),
+      Return(v3)
+    )
+    assertEquals(bb3, expectedBB3)
   }
 
 }
